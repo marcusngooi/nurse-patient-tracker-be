78add817cab8ad462cb0c19661acaa4d85b3c99f
Marcus Ngooi <mngooi@gmail.com>
2025-03-13 22:31:14 -0400

    [MOD]Debugging

--- config/app.js
+++ config/app.js
@@ -21,7 +21,7 @@ app.use(morgan("dev"));
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use((res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
